682ffecd384b585f385e989b5828d3dd2b21b752
Faith Igomodu <168231295+FaithIgomodu@users.noreply.github.com>
2025-11-21 00:07:13 -0500

    Script to start Azure VM
    Adds a script to start an Azure virtual machine.

---
+++ azure/startvm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Author: Faith Igomodu
+#Description: Starts an Azure virtual machine.  
+#Date: Nov. 18, 2025 
+
+
+az vm start --resource-group <resource_group_name> --name <name_of_virtual_machine>
