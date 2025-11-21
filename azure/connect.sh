3408c277b72034d241f7bf53fba7dcf1f115c940
Faith Igomodu <168231295+FaithIgomodu@users.noreply.github.com>
2025-11-20 23:57:39 -0500

    SSH connection script

---
+++ azure/connect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Author: Faith Igomodu
+#Description: Connects to an Azure virtual machine using SSH. 
+#Date: Nov. 18, 2025
+
+#Connect to a virtual machine
+ssh -i <path/to/ssh/key> azureuser@02.115.04.100
